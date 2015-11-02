57895bf17a45d6fce31bdb3f36a53e4b4af9a57a
ThibaultCam <cam@intechinfo.fr>
2015-11-02 12:15:50 +0100

    add GUI (désolé)

---
+++ ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End.GUI/EscapeWindow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITI.S3.PI.Chick_End.GUI
+{
+    public partial class EscapeWindow : Form
+    {
+        Play _window;
+        public EscapeWindow(Play window)
+        {
+            InitializeComponent();
+            _window = window;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quit(object sender, EventArgs e)
+        {
+            using (Form m = new Menu())
+            {
+                m.ShowDialog();
+                _window.Close();
+                Close();
+            }
+        }
+
+        private void continueGame(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End.GUI/Play.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ITI.S3.PI.Chick_End.GUI
+{
+    partial class Play
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Play));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(148, 420);
+            this.panel1.TabIndex = 0;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(3, 334);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(142, 67);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Hen";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(3, 261);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(142, 67);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Hen";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 188);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(142, 67);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Hen";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 115);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 67);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Hen";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 67);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Hen";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Playbill", 26.25F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(44, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Units";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(549, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Menu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.escapeWindow);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(480, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Trash";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Play
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(624, 420);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Play";
+            this.Text = "Play";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
