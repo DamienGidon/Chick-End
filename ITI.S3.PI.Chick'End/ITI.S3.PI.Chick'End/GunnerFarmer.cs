ea8e0be814320731ad0366e4b981ed5c65173cbb
LoicMonard <monard@intechinfo.fr>
2015-11-02 14:42:35 +0100

    Add every towers

---
+++ ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End/GunnerFarmer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.S3.PI.Chick_End
+{
+    class GunnerFarmer : Tower
+    {
+        internal GunnerFarmer()
+            : base(10, 15)
+        {
+            _health = 50;
+        }
+
+        public override void Attack(Ennemi ennemi)
+        {
+            ennemi.Health -= Damages;
+        }
+        
+        /*
+        public override void Remove()
+        {
+
+        }
+        */
+    }
+}
