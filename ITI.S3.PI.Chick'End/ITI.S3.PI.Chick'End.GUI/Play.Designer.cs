d50a9e72669bd08a227c000ad6e38d4669907ef6
ThibaultCam <cam@intechinfo.fr>
2015-11-02 14:50:50 +0100

    Update GUI

--- ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End.GUI/Play.Designer.cs
+++ ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End.GUI/Play.Designer.cs
@@ -38,7 +38,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -57,7 +60,7 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(3, 334);
+            this.button7.Location = new System.Drawing.Point(3, 350);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(142, 67);
             this.button7.TabIndex = 7;
@@ -66,7 +69,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(3, 261);
+            this.button6.Location = new System.Drawing.Point(3, 277);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(142, 67);
             this.button6.TabIndex = 6;
@@ -75,7 +78,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(3, 188);
+            this.button5.Location = new System.Drawing.Point(3, 204);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(142, 67);
             this.button5.TabIndex = 5;
@@ -84,7 +87,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(3, 115);
+            this.button4.Location = new System.Drawing.Point(3, 131);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(142, 67);
             this.button4.TabIndex = 4;
@@ -93,7 +96,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(3, 42);
+            this.button3.Location = new System.Drawing.Point(3, 58);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(142, 67);
             this.button3.TabIndex = 3;
@@ -105,7 +108,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Playbill", 26.25F);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.label1.Location = new System.Drawing.Point(44, 4);
+            this.label1.Location = new System.Drawing.Point(45, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 35);
             this.label1.TabIndex = 0;
@@ -114,9 +117,9 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(549, 12);
+            this.button1.Location = new System.Drawing.Point(549, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(63, 45);
+            this.button1.Size = new System.Drawing.Size(63, 33);
             this.button1.TabIndex = 1;
             this.button1.Text = "Menu";
             this.button1.UseVisualStyleBackColor = true;
@@ -125,12 +128,34 @@
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(480, 12);
+            this.button2.Location = new System.Drawing.Point(480, 6);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(63, 45);
+            this.button2.Size = new System.Drawing.Size(63, 33);
             this.button2.TabIndex = 2;
             this.button2.Text = "Trash";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(144, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(480, 60);
+            this.panel2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Playbill", 26.25F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(10, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 35);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Seeds :";
             // 
             // Play
             // 
@@ -142,11 +167,14 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Play";
             this.Text = "Play";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -162,5 +190,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
     }
 }
