38f7470dca6d51d25e1eca2e19465790923905d8
ThibaultCam <cam@intechinfo.fr>
2015-11-13 10:05:22 +0100

    New GUI

---
+++ ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End.GUI/FormF.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ITI.S3.PI.Chick_End.GUI
+{
+    partial class FormF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormF));
+            this.MenuPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonContinue = new System.Windows.Forms.Button();
+            this.buttonBestiary = new System.Windows.Forms.Button();
+            this.buttonLeaderboard = new System.Windows.Forms.Button();
+            this.buttonRules = new System.Windows.Forms.Button();
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.MenuPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuPanel
+            // 
+            this.MenuPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MenuPanel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MenuPanel.BackgroundImage")));
+            this.MenuPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.MenuPanel.Controls.Add(this.buttonQuit);
+            this.MenuPanel.Controls.Add(this.buttonRules);
+            this.MenuPanel.Controls.Add(this.buttonLeaderboard);
+            this.MenuPanel.Controls.Add(this.buttonBestiary);
+            this.MenuPanel.Controls.Add(this.buttonContinue);
+            this.MenuPanel.Controls.Add(this.button1);
+            this.MenuPanel.Location = new System.Drawing.Point(1, 0);
+            this.MenuPanel.Name = "MenuPanel";
+            this.MenuPanel.Size = new System.Drawing.Size(1007, 728);
+            this.MenuPanel.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(453, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonContinue
+            // 
+            this.buttonContinue.Location = new System.Drawing.Point(453, 237);
+            this.buttonContinue.Name = "buttonContinue";
+            this.buttonContinue.Size = new System.Drawing.Size(75, 23);
+            this.buttonContinue.TabIndex = 1;
+            this.buttonContinue.Text = "Continue";
+            this.buttonContinue.UseVisualStyleBackColor = true;
+            // 
+            // buttonBestiary
+            // 
+            this.buttonBestiary.Location = new System.Drawing.Point(453, 286);
+            this.buttonBestiary.Name = "buttonBestiary";
+            this.buttonBestiary.Size = new System.Drawing.Size(75, 23);
+            this.buttonBestiary.TabIndex = 2;
+            this.buttonBestiary.Text = "Bestiary";
+            this.buttonBestiary.UseVisualStyleBackColor = true;
+            // 
+            // buttonLeaderboard
+            // 
+            this.buttonLeaderboard.Location = new System.Drawing.Point(453, 337);
+            this.buttonLeaderboard.Name = "buttonLeaderboard";
+            this.buttonLeaderboard.Size = new System.Drawing.Size(75, 23);
+            this.buttonLeaderboard.TabIndex = 3;
+            this.buttonLeaderboard.Text = "Leaderboard";
+            this.buttonLeaderboard.UseVisualStyleBackColor = true;
+            // 
+            // buttonRules
+            // 
+            this.buttonRules.Location = new System.Drawing.Point(453, 395);
+            this.buttonRules.Name = "buttonRules";
+            this.buttonRules.Size = new System.Drawing.Size(75, 23);
+            this.buttonRules.TabIndex = 4;
+            this.buttonRules.Text = "Rules";
+            this.buttonRules.UseVisualStyleBackColor = true;
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Location = new System.Drawing.Point(453, 446);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuit.TabIndex = 5;
+            this.buttonQuit.Text = "Quit";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // FormF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.MenuPanel);
+            this.Name = "FormF";
+            this.Text = "FormF";
+            this.MenuPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MenuPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonQuit;
+        private System.Windows.Forms.Button buttonRules;
+        private System.Windows.Forms.Button buttonLeaderboard;
+        private System.Windows.Forms.Button buttonBestiary;
+        private System.Windows.Forms.Button buttonContinue;
+    }
+}
---
+++ ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End.GUI/FormF.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITI.S3.PI.Chick_End.GUI
+{
+    public partial class FormF : Form
+    {
+        public FormF()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            MenuPanel.Hide();
+            this.Controls.Add(new gamecontrol(MenuPanel));
+        }
+
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
