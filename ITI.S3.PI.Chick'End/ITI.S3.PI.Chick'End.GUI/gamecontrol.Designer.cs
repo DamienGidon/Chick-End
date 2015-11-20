4cbbf82cc5dc17c7bfcf736b0d532382f4dd7188
Fx Suarez <francoisxaviersuarez@gmail.com>
2015-11-20 09:33:23 +0100

    Remove code associated to an unexisting checkbox

--- ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End.GUI/gamecontrol.Designer.cs
+++ ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End.GUI/gamecontrol.Designer.cs
@@ -249,7 +249,6 @@
             this.pictureBox1.Size = new System.Drawing.Size(62, 61);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_MouseDown);
             // 
             // button1
             // 
