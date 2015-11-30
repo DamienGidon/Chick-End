c3377b02c426043529576c963134024d2a6cf559
Fx Suarez <francoisxaviersuarez@gmail.com>
2015-11-30 11:53:29 +0100

    fix pull errors

--- ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End.GUI/Bestiary.cs
+++ ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End.GUI/Bestiary.cs
@@ -33,18 +33,5 @@ namespace ITI.S3.PI.Chick_End.GUI
             buttonReturn.Font = new System.Drawing.Font("Playbill", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             buttonReturn.Refresh();
         }
-
-        protected override void OnMouseWheel(MouseEventArgs e)
-        {
-           base.OnMouseWheel(e);
-        }
-
-        private void panelBestiary_OnMouseWheel(object sender, MouseEventArgs e)
-        {
-            AutoScroll.Scroll += new ScrollEventHandler(
-            this.AutoSroll_Scroll);
-            vScrollBar1.ValueChanged += new EventHandler(
-            this.vScrollBar1_ValueChanged);
-        }
     }
 }
