dd12db1b3c99609ee78f44727cde2fb16892d686
ThibaultCam <cam@intechinfo.fr>
2015-12-11 09:48:53 +0100

    Add GameOver Control

---
+++ ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End.GUI/GameOver.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ITI.S3.PI.Chick_End.GUI
+{
+    partial class GameOver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonMenu = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Playbill", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Orange;
+            this.label1.Location = new System.Drawing.Point(44, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 98);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Game Over";
+            // 
+            // buttonMenu
+            // 
+            this.buttonMenu.FlatAppearance.BorderSize = 0;
+            this.buttonMenu.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMenu.Font = new System.Drawing.Font("Playbill", 20F);
+            this.buttonMenu.ForeColor = System.Drawing.Color.Orange;
+            this.buttonMenu.Location = new System.Drawing.Point(143, 154);
+            this.buttonMenu.Name = "buttonMenu";
+            this.buttonMenu.Size = new System.Drawing.Size(88, 44);
+            this.buttonMenu.TabIndex = 1;
+            this.buttonMenu.Text = "Menu";
+            this.buttonMenu.UseVisualStyleBackColor = true;
+            this.buttonMenu.Click += new System.EventHandler(this.buttonMenu_Click);
+            this.buttonMenu.MouseEnter += new System.EventHandler(this.buttonMenu_MouseEnter);
+            this.buttonMenu.MouseLeave += new System.EventHandler(this.buttonMenu_MouseLeave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 15.25F);
+            this.label2.Location = new System.Drawing.Point(103, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Score :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Rockwell", 15.25F);
+            this.label3.Location = new System.Drawing.Point(198, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "{score}";
+            // 
+            // GameOver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SaddleBrown;
+            this.ClientSize = new System.Drawing.Size(370, 210);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonMenu);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "GameOver";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "GameOver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonMenu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End.GUI/GameOver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITI.S3.PI.Chick_End.GUI
+{
+    public partial class GameOver : Form
+    {
+        public GameOver()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonMenu_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonMenu_MouseEnter(object sender, EventArgs e)
+        {
+            buttonMenu.Font = new System.Drawing.Font("Playbill", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            buttonMenu.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            buttonMenu.Refresh();
+        }
+
+        private void buttonMenu_MouseLeave(object sender, EventArgs e)
+        {
+            buttonMenu.Font = new System.Drawing.Font("Playbill", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            buttonMenu.Refresh();
+        }
+    }
+}
