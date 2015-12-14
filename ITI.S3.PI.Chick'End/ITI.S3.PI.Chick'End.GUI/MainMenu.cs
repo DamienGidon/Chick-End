644f8b140da1ea7eb58de6ae3ce385ff9d067c27
ThibaultCam <cam@intechinfo.fr>
2015-12-14 09:35:29 +0100

    spi modif in progress

---
+++ ITI.S3.PI.Chick'End/ITI.S3.PI.Chick'End.GUI/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITI.S3.PI.Chick_End.GUI
+{
+    public partial class MainMenu : UserControl
+    {
+        IGameContext _context;
+
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        IGameContext GameContext { get { return (IGameContext)FindForm(); } }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            if( _context.CurrentGame == null 
+                || MessageBox.Show("Are you sure?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                _context.CurrentGame = new Game();
+                Hide();
+            }
+        }
+    }
+}
